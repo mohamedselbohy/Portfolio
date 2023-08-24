067ac0193aa89efba67e65dc83e17a18e6046127
mohamedselbohy <msayedelbohy@hotmail.com>
2023-08-24 04:33:58 +0300

    added services section and added links for it in the nav-bar

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,27 @@ const navObserver = new IntersectionObserver((entries,obsverver)=>{
     }
 },{});
 navObserver.observe(topSection);
+let skills = document.getElementsByClassName('skill-box');
+selected=[0,0,0];
+for(let i=0;i<skills.length;i++)
+{
+    skills[i].addEventListener('click',()=>{
+            for(let j=0;j<skills.length;j++)
+            {
+                skills[j].classList.remove('clicked-skill-box');
+                skills[j].classList.add('unskill-box');
+            }
+            if(!selected[i]){
+            skills[i].classList.add('clicked-skill-box');
+            skills[i].classList.remove('unskill-box');
+            selected[i]=1;
+            selected[i+1]=0;
+            selected[i+2]=0;
+            selected[i-1]=0;
+            selected[i-2]=0;
+            }
+            else{
+                selected[i]=0;
+            }
+    })
+}
